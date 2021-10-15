b0999a602d3ddbac84e2d051896a4d1ebe538a2d
Jamie Phillips <cphill11@gmail.com>
2021-10-14 21:38:47 -0400

    Basic magefile and setup.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module mage-example
+
+go 1.16
+
+require github.com/magefile/mage v1.11.0
---
+++ magefile.go
@@ -0,0 +1,37 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+    "path/filepath"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"	
+)
+
+var Default = Build
+
+func Clean() error {
+	return sh.Rm("bin")
+}
+
+func Build() error {
+	mg.Deps(Clean)
+	if err := sh.Run("go", "mod", "download"); err != nil {
+		return err
+	}
+	return sh.Run("go", "build", "-o", "./bin/")
+}
+
+func Download() error {
+	mg.Deps(Build)
+
+	url := "https://gist.githubusercontent.com/phillipsj/07fed8ce06f932c19ab7613d8426d922/raw/13d3fc0ca54d136ad5744fd4448b65dbc87f32dc/random.txt"
+	url := "https://gist.githubusercontent.com/phillipsj/07fed8ce06f932c19ab7613d8426d922/raw/13d3fc0ca54d136ad5744fd4448b65dbc87f32dc/random.txt"
+	err := sh.Run("curl", "-O", url)
+	if err != nil {
+		return err
+	}
+	return sh.Copy(filepath.Join("bin", "random.txt"), "random.txt")
+}
