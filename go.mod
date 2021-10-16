158072a03094d036ccbbe57d1a6086343f6d2b3b
Jamie Phillips <cphill11@gmail.com>
2021-10-16 14:56:23 -0400

    Moving the GetFile function to a package and consuming it.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module mage-example
 
 go 1.16
 
-require github.com/magefile/mage v1.11.0
+require (
+	github.com/magefile/mage v1.11.0
+	github.com/phillipsj/mage-helpers v0.0.0-20211016184917-42c11b58d972 // indirect
+)
--- magefile.go
+++ magefile.go
@@ -4,13 +4,11 @@
 package main
 
 import (
-	"io"
-	"net/http"
-	"os"
 	"path/filepath"
 
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
+	"github.com/phillipsj/mage-helpers/dl"
 )
 
 var Default = Build
@@ -31,25 +29,5 @@ func Download() error {
 	mg.Deps(Build)
 
 	url := "https://gist.githubusercontent.com/phillipsj/07fed8ce06f932c19ab7613d8426d922/raw/13d3fc0ca54d136ad5744fd4448b65dbc87f32dc/random.txt"
-	return GetFile(url, filepath.Join("bin", "random.txt"))
-}
-
-func GetFile(url string, file string) error {
-	out, err := os.Create(file)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if _, err := io.Copy(out, resp.Body); err != nil {
-		return err
-	}
-
-	return nil
+	return dl.GetFile(url, filepath.Join("bin", "random.txt"))
 }
